c05360c76586028aacb210e2d466dd1f8d1a7327
Meio <meiplesyrup@gmail.com>
2019-04-07 20:32:51 +0800

    Header mobile refactor and cleaned up animations ^O^

--- public/js/header.js
+++ public/js/header.js
@@ -9,12 +9,12 @@ let logoTextIsClosed = false;
 function hideLogoText() {
   let isScrollTopZero = !(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
   if (!isScrollTopZero && !logoTextIsClosed) {
-    document.querySelector(".logo_text").className = "logo_text--closed"
+    document.querySelector(".logo__text").className = "logo__text--closed"
     document.querySelector(".nav_logo").className = "nav_logo--open global_flex-center"
     document.querySelector(".nav_main-container").className = "nav_main-container--open global_flex-center"
     logoTextIsClosed = true;
   } else if (isScrollTopZero && logoTextIsClosed) {
-    document.querySelector(".logo_text--closed").className = "logo_text"
+    document.querySelector(".logo__text--closed").className = "logo__text"
     document.querySelector(".nav_logo--open").className = "nav_logo global_flex-center"
     document.querySelector(".nav_main-container--open").className = "nav_main-container global_flex-center"
     logoTextIsClosed = false;
@@ -28,12 +28,12 @@ let isMenuActive = false;
 function clickMenu() {
   if (!isMenuActive) {
     document.querySelector(".nav_menu").className = "nav_menu--active"
-    document.querySelector(".nav_menu-fullscreen").className = "nav_menu-fullscreen--active"
+    document.querySelector(".nav_menu-fullscreen").className = "mobile nav_menu-fullscreen--active"
     document.querySelector("header.mobile .nav_logo").className = "nav_logo global_display-none"
     isMenuActive = true;
   } else if (isMenuActive) {
     document.querySelector(".nav_menu--active").className = "nav_menu"
-    document.querySelector(".nav_menu-fullscreen--active").className = "nav_menu-fullscreen"
+    document.querySelector(".nav_menu-fullscreen--active").className = "mobile nav_menu-fullscreen"
     document.querySelector("header.mobile .nav_logo").className = "nav_logo global_flex-center"
     isMenuActive = false;
   }
