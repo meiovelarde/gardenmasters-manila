24bdde420aa3bbf7ad68cf7c938d26abb6c35e19
Meio <meiplesyrup@gmail.com>
2019-07-15 05:20:29 +0800

    Font fix mobile until mw:750px. TODO: desktop & tablet fnt, refactor css

--- public/js/modules/clients.mjs
+++ public/js/modules/clients.mjs
@@ -56,10 +56,21 @@ export let M_CLIENTS = {
 			x = e.clientX;
 			y = e.clientY;
 		}
-
-		window.addEventListener('touchstart', touch);
-		window.addEventListener('touchmove', touch);
-		window.addEventListener('mousemove', mousemove);
+    let setupListeners = () => {
+      console.log(S_isMobile())
+      if (!S_isMobile()){
+        window.addEventListener('touchstart', touch);
+        window.addEventListener('touchmove', touch);
+        window.addEventListener('mousemove', mousemove);
+      } else {
+        window.removeEventListener('touchstart', touch);
+        window.removeEventListener('touchmove', touch);
+        window.removeEventListener('mousemove', mousemove);
+      }
+    }
+    setupListeners();
+    window.addEventListener('resize', setupListeners)
+    window.addEventListener('sizemodechange', setupListeners)
 		requestAnimationFrame(renderCursor);
 
 		const computeCursorPos = () => {
