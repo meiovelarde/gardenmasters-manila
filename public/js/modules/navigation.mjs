d0270a690a016fbdfdddf4b9859ea97ee3d3a4e0
Meio <meiplesyrup@gmail.com>
2019-07-16 04:54:50 +0800

    Back to top

--- public/js/modules/navigation.mjs
+++ public/js/modules/navigation.mjs
@@ -1,5 +1,40 @@
+import { S_isMobile, S_ifElemInViewport } from './shared.mjs';
 export const M_NAV = {
 	init() {
+		//Begin to top
+		const buttonToTop = document.querySelector('.button_to-top');
+		const toggleTopButtonActive = (isActive) => {
+			buttonToTop.dataset.isActive = isActive
+		}
+		const handleTopButtonClick = () => {
+			if(buttonToTop.dataset.isActive){
+				window.scrollTo({
+					top: 0,
+					left: 0,
+					behavior: 'smooth'
+				})
+				toggleTopButtonActive('false')
+			}
+		}
+		const checkTopButtonActive = () => {
+			const atf = document.querySelector('.atf__bottom-hedge')
+			if(!S_ifElemInViewport(atf))
+				toggleTopButtonActive('true')
+			else
+				toggleTopButtonActive('false')
+		}
+
+    const setupTouch = () => {
+      if(S_isMobile())
+        buttonToTop.dataset.isTouch = 'true'
+      else buttonToTop.dataset.isTouch = 'false'
+    }
+
+    setupTouch();
+		buttonToTop.addEventListener('mousedown', handleTopButtonClick)
+		window.addEventListener('scroll', checkTopButtonActive)
+    window.addEventListener('resize', setupTouch)
+    window.addEventListener('sizemodechange', setupTouch)
     // window.scrollTo(0, 0);
     const SEC_CLIENTS = document.querySelector('section.clients'),
           SEC_SERVICES = document.querySelector('section.services'),
